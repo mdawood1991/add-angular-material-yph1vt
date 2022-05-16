a0302a44546abe1f3d512c2ecb4b397ccc6683eb
mdawood1991 <mdawood1991@gmail.com>
2022-05-16 22:16:42 +0100

    Common Input component

--- src/app/common-modal/common-modal.component.ts
+++ src/app/common-modal/common-modal.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+export interface IModalConfig {
+  title: string;
+  dismissButtonLabel?: string;
+  closeButtonLabel?: string;
+  shouldClose?(): Promise<boolean> | boolean;
+  shouldDismiss?(): Promise<boolean> | boolean;
+  onClose?(): Promise<boolean> | boolean;
+  onDismiss?(): Promise<boolean> | boolean;
+  disableCloseButton?(): boolean;
+  disableDismissButton?(): boolean;
+  hideCloseButton?(): boolean;
+  hideDismissButton?(): boolean;
+}
 
 @Component({
   selector: 'app-common-modal',
   templateUrl: './common-modal.component.html',
-  styleUrls: ['./common-modal.component.css']
+  styleUrls: ['./common-modal.component.css'],
 })
 export class CommonModalComponent implements OnInit {
+  @Input() public modalConfig: IModalConfig;
+  @Input() public formModal: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSave() {
+    // TODO: POST TO SERVER, HANDLE VALIDATION ERRORS, HANDLE SUCCESS!
   }
-
 }
--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IModalConfig } from '../common-modal/common-modal.component';
 
 @Component({
   selector: 'app-my-form',
   templateUrl: './my-form.component.html',
-  styleUrls: ['./my-form.component.css']
+  styleUrls: ['./my-form.component.css'],
 })
 export class MyFormComponent implements OnInit {
+  constructor(private formBuilder: FormBuilder) {}
 
-  constructor() { }
+  formModal: FormGroup = this.formBuilder.group({
+    name: this.formBuilder.control(null, [Validators.required]),
+  });
+  modalConfig: IModalConfig;
 
   ngOnInit() {
+    this.modalConfig = {
+      title: 'Create Vehicle',
+    };
   }
-
 }
